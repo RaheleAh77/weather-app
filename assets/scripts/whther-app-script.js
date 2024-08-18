84da7ab5c209a4fb968b83a9bc51f462d9d7de23
Rahele <108678700+RaheleAh77@users.noreply.github.com>
2024-08-19 02:06:29 +0330

    create whether app with random background

---
+++ assets/scripts/whther-app-script.js
@@ -0,0 +1,43 @@
+let imgsArray = ["../image/cloudywhether", "../image/bridge", "../image/freedom", "../image/sunshine", "../image/beach", "../image/flower-1", "../image/flower-2"];
+let randomImgIndex;
+
+setInterval(function () {
+  randomImgIndex = Math.floor(Math.random() * imgsArray.length);
+  document.body.style.backgroundImage = "url(imgsArray[randomImgIndex])";
+}, 3000);
+
+let citiesData = [
+  { city: "Tehran", temp: 12, weather: "Sunny", humidity: 23, windSpeed: 32 },
+  { city: "Shiraz", temp: 9, weather: "windy", humidity: 12, windSpeed: 14 },
+  { city: "Tabriz", temp: 1, weather: "rainy", humidity: 43, windSpeed: 12 },
+  { city: "Mashhad", temp: 16, weather: "snowy", humidity: 7, windSpeed: 24 },
+  { city: "Esfahan", temp: 23, weather: "Sunny", humidity: 15, windSpeed: 18 },
+];
+
+let $ = document;
+let searchBtn = $.getElementById("search");
+let searchBar = $.querySelector(".search-bar");
+
+searchBtn.addEventListener("click", function () {
+  let searchBarValue = searchBar.value; // tabriz
+  // let mainCityDatas = citiesData[searchBarValue]
+
+  let mainCityDatas = citiesData.find(function (item) {
+    return item.city === searchBarValue;
+  });
+
+  console.log(mainCityDatas);
+
+  if (mainCityDatas) {
+    $.querySelector(".city").innerHTML = "Weather in " + mainCityDatas.city;
+    $.querySelector(".temp").innerHTML = mainCityDatas.temp + "°C";
+    $.querySelector(".description").innerHTML = mainCityDatas.weather;
+    $.querySelector(".humidity").innerHTML =
+      "Humidity: " + mainCityDatas.humidity;
+    $.querySelector(".wind").innerHTML =
+      "Wind speed: " + mainCityDatas.windSpeed + "km/h";
+    $.querySelector(".weather").classList.remove("loading");
+  } else {
+    alert("شهر مورد نظر را به درستی وارد نمایید");
+  }
+});
